df1f8353fd00af55e881ec95d9d5ef4aaf2077ef
Konstantinos Kaplanis <contact@kostaskaplanis.gr>
2025-10-04 12:56:43 +0300

    django-silk Profiling and Optimization

--- drf_course/urls.py
+++ drf_course/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+    path('', include('api.urls')),
+    path('silk/', include('silk.urls', namespace='silk'))
 ]
+
