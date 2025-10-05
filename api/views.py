5bb5846e34d696bbbbdb797eade19920f899697c
Konstantinos Kaplanis <contact@kostaskaplanis.gr>
2025-10-05 10:59:57 +0300

    add isAuthenticated permission to generic view

--- api/views.py
+++ api/views.py
@@ -7,6 +7,8 @@ from api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
+# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ProductListAPIView(generics.ListAPIView): 
     queryset = Product.objects.all()
@@ -36,7 +38,9 @@ class OrderListAPIView(generics.ListAPIView):
 class UserOrderListAPIView(generics.ListAPIView): 
     queryset = Order.objects.prefetch_related('items__product')
     serializer_class = OrderSerializer
-    
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get_queryset(self): 
         qs = super().get_queryset()
         return qs.filter(user=self.request.user)
