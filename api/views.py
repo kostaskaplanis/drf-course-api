d45fbcc6ab62149577537e8ee35c87812b3d80c5
Konstantinos Kaplanis <contact@kostaskaplanis.gr>
2026-02-27 08:29:10 +0200

    Update API views and starter code changes

--- api/views.py
+++ api/views.py
@@ -41,10 +41,16 @@ class ProductCreateAPIView(generics.CreateAPIView):
 #     serializer = ProductSerializer(products, many=True)
 #     return Response(serializer.data)
 
-class ProductDetailAPIView(generics.RetrieveAPIView): 
+class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView): 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_url_kwarg='product_id'
+
+    def get_permissions(self): 
+        self.permission_classes = [AllowAny]
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']: 
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 # @api_view(['GET'])
 # def product_detail(request, pk): 
 #     product = get_object_or_404(Product, pk=pk)
