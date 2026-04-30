b8603974168e66a10377aa306843e44974c0b2f4
Konstantinos Kaplanis <contact@kostaskaplanis.gr>
2026-04-30 10:25:34 +0300

    configuring PageNumberPagination

--- api/views.py
+++ api/views.py
@@ -15,13 +15,14 @@ IsAuthenticated, IsAdminUser, AllowAny
 )
 from rest_framework import filters 
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
 
 class ProductListAPIView(generics.ListAPIView): 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductListCreateAPIView(generics.ListCreateAPIView): 
-    queryset = Product.objects.all()
+    queryset = Product.objects.order_by('pk')
     serializer_class = ProductSerializer
     filterset_class = ProductFilter
     filter_backends = [DjangoFilterBackend,
@@ -31,6 +32,11 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
                        ]
     search_fields = ['name','description']
     ordering_fields = ['name', 'price', 'stock']
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 2
+    pagination_class.page_query_param = 'pagenum'
+    pagination_class.page_size_query_param = 'size'
+    pagination_class.max_page_size = 6
     
     def get_permissions(self): 
         self.permission_classes = [AllowAny]
